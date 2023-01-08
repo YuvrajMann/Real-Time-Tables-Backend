8ff5de9b17ae1969a6c7425204b06ca588688151
Yuvraj <yuvraj1mann@gmail.com>
2023-01-08 15:30:26 +0530

    dbAdded

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
   secretKey: "12345-67890-09876-54321",
   mongoUrl: "mongodb://127.0.0.1:27017/conFusion",
   cloudUrl:
-    "mongodb+srv://yuvrajmann:encyclopedi@cluster0.ow6fw.mongodb.net/conFusion?retryWrites=true&w=majority",
+    "mongodb://realtimetablesdb:mDlNMz84W2wclnb15TCsPpbn707J5lsTGkDc1pCcikCeC4VNIWstbAxqZgaVYMGsOBfLoX3kdEXZACDbYbiYpg==@realtimetablesdb.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@realtimetablesdb@",
 };
